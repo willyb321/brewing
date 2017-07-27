add645be488c4767720801033b7af51f76865e26
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-28 09:05:28 +1000

    gthsManage version v1.0.0

---
+++ gthsManage.rb
@@ -0,0 +1,15 @@
+class GthsManage < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/willyb321/gthsManage/releases/download/v1.0.0/gthsManage_1.0.0_macOS_x64.zip"
+  version "1.0.0"
+  sha256 "b62636ee825f6fba3a7a920aec94bdd1895f0d003f27d9720f7e2c8845380ae9"
+
+  def install
+    bin.install "gthsManage"
+  end
+
+  test do
+    
+  end
+end
