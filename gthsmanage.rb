18461df9f19911749defbf80609877d839f7b7e3
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-04 09:31:07 +1000

    gthsmanage version v1.1.4

--- gthsmanage.rb
+++ gthsmanage.rb
@@ -1,9 +1,9 @@
 class Gthsmanage < Formula
   desc "Software to keep the GTHS Noticeboard running."
   homepage "https://github.com/willyb321/gthsmanage/"
-  url "https://github.com/willyb321/gthsmanage/releases/download/v1.1.3/gthsmanage_1.1.3_macOS_x64.zip"
-  version "1.1.3"
-  sha256 "3f0979317e4dfd855de9dafc52612b9fc2169147c5092be324a6505d1898c07e"
+  url "https://github.com/willyb321/gthsmanage/releases/download/v1.1.4/gthsmanage_1.1.4_macOS_x64.zip"
+  version "1.1.4"
+  sha256 "fc13c79cf5d8c18f9a2eb141c19a5b45754c1f1e98207af918ab867c64d15996"
 
   def install
     bin.install "gthsmanage"
